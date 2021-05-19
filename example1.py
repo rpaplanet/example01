d28428a248fbdd3535b83649d9cb6e739c5cf545
yogmitta <yogesh2.mittal@aricent.com>
2021-05-19 12:24:43 +0530

    ''

--- example1.py
+++ example1.py
@@ -24,6 +24,7 @@ try:
         cp = cmd.run("git status", check=True, shell=True)
         cp = cmd.run("git add .", check=True, shell=True)
         cp = cmd.run(" git commit --allow-empty-message -m '' ", check=True, shell=True)
+        print(cp)
         cp = cmd.run("git push -u origin main ", check=True, shell=True)
         print("thanks! result is updated successfully")
     elif value.lower() == "n":
